7641fee87c6293a1bfd3812abd1ea7a170086900
DIOVANY BRYAN GUNAWAN <diovany.gunawan@pegadaian.co.id>
2024-10-03 23:10:25 +0700

    feat: styling tasbih

--- lib/presentation/feature/tasbih/tasbih_screen.dart
+++ lib/presentation/feature/tasbih/tasbih_screen.dart
@@ -33,15 +33,49 @@ class _TasbihScreenState extends State<TasbihScreen> { // Ganti State class
   'Ya Hayyu Ya Qayyum يَا حَيُّ يَا قَيُّومُ Wahai Yang Maha Hidup, Yang Maha Berdiri Sendiri'
 ];
 
+  double _opacity = 1.0; // Menyimpan nilai opacity
+
   void incrementCounter() {
     setState(() {
-      if (counter >= 33) {
-        counter = 1; // Reset to 1 if counter reaches 33
-        randomText = (texts..shuffle()).first; // Change to a random text
-      } else {
-        counter++;
+      counter++;
+      if (counter > 33) { // Reset counter jika lebih dari 33
+        counter = 1;
+        randomText = getRandomString(); // Memperbarui randomText saat reset
       }
+      _opacity = 0.0; // Mengatur opacity menjadi 0 untuk efek menyala
     });
+
+    // Mengembalikan opacity setelah delay
+    Future.delayed(Duration(milliseconds: 300), () {
+      setState(() {
+        _opacity = 1.0; // Mengembalikan opacity ke 1
+      });
+    });
+  }
+
+  // Fungsi untuk mendapatkan string random
+  String getRandomString() {
+    // Ganti dengan logika untuk mendapatkan string random dari daftar
+    List<String> strings = [
+  'Allahu Akbar الله أكبر Allah Maha Besar',
+  'Astagfirullah أستغفر الله Aku memohon ampun kepada Allah',
+  'Alhamdulillah الحمد لله Segala puji bagi Allah',
+  'La ilaha illallah لا إله إلا الله Tiada Tuhan selain Allah',
+  'Subhanallah سبحان الله Maha Suci Allah',
+  'Hasbiyallah حسبي الله Cukuplah Allah bagiku',
+  'La hawla wa la quwwata illa billah لا حول ولا قوة إلا بالله Tiada daya dan kekuatan kecuali dengan Allah',
+  'Subhanallahi wa bihamdihi سبحان الله وبحمده Maha Suci Allah dan segala puji bagi-Nya',
+  'Subhanallahil \'Azim سبحان الله العظيم Maha Suci Allah yang Maha Agung',
+  'Bismillah بسم الله Dengan nama Allah',
+  'Innalillahi wa inna ilaihi raji\'un إِنَّا لِلّهِ وَإِنَّـا إِلَيْهِ رَاجِعونَ Sesungguhnya kami milik Allah dan kepada-Nya kami kembali',
+  'Allahumma salli \'ala Muhammad اللَّهُمَّ صَلِّ عَلَى مُحَمَّدٍ Ya Allah, limpahkanlah shalawat atas Nabi Muhammad',
+  'Raditu billahi rabba رَضِيتُ بِاللهِ رَبًّا Aku ridha Allah sebagai Tuhanku',
+  'Ya Allah يا الله Wahai Allah',
+  'La ilaha illa anta subhanaka inni kuntu minaz zalimin لَا إِلَهَ إِلَّا أَنتَ سُبْحَانَكَ إِنِّي كُنتُ مِنَ الظَّالِمِينَ Tidak ada Tuhan selain Engkau, Maha Suci Engkau, sesungguhnya aku termasuk orang yang zalim',
+  'Alhamdulillahi rabbil \'alamin الحمد لله رب العالمين Segala puji bagi Allah, Tuhan seluruh alam',
+  'Ya Hayyu Ya Qayyum يَا حَيُّ يَا قَيُّومُ Wahai Yang Maha Hidup, Yang Maha Berdiri Sendiri'
+]; // Contoh daftar string
+    return (strings..shuffle()).first; // Mengacak dan mengambil string pertama
   }
 
   Future<void> initLocal() async {
@@ -60,10 +94,11 @@ class _TasbihScreenState extends State<TasbihScreen> { // Ganti State class
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green[100], // Mengatur warna background
+      backgroundColor: Colors.white, // Mengatur warna latar belakang menjadi putih
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text("Tasbih Digital"), // Ganti judul
+        backgroundColor: Colors.white, // Mengubah warna header menjadi putih
+        title: Text("Dzikir Digital", style: TextStyle(color: Colors.black)), // Mengubah warna teks judul menjadi hitam
+        iconTheme: IconThemeData(color: Colors.green), // Mengubah warna tombol back menjadi hijau
       ),
       body: Stack( // Changed from Column to Stack
         children: [
@@ -75,20 +110,44 @@ class _TasbihScreenState extends State<TasbihScreen> { // Ganti State class
                   'Mulai Berdzikir',
                   style: Theme.of(context).textTheme.headlineLarge?.copyWith(
                     fontWeight: FontWeight.bold, // Make text bold
-                    fontSize: (Theme.of(context).textTheme.headlineLarge?.fontSize ?? 20) + 2, // Increase font size by 2
+                    fontSize: (Theme.of(context).textTheme.headlineLarge?.fontSize ?? 20) + 4, // Increase font size by 4
+                    color: Colors.black, // Change text color to black
                   ),
                   textAlign: TextAlign.center, // Center the text
                 ),
                 const SizedBox(height: 16), // Add spacing between the title and counter
-                Text(
-                  '$counter',
-                  style: Theme.of(context).textTheme.headlineMedium,
-                  textAlign: TextAlign.center, // Center the text
+                AnimatedOpacity(
+                  opacity: _opacity, // Menggunakan nilai opacity
+                  duration: Duration(milliseconds: 300), // Durasi animasi
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.green, // Warna latar belakang lingkaran
+                      shape: BoxShape.circle, // Bentuk lingkaran
+                    ),
+                    padding: EdgeInsets.all(20), // Padding untuk memberi ruang di dalam lingkaran
+                    child: Text(
+                      '$counter',
+                      style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                        color: Colors.white, // Mengubah warna teks menjadi putih
+                        shadows: [
+                          Shadow(
+                            blurRadius: 10.0,
+                            color: Colors.black.withOpacity(0.5),
+                            offset: Offset(2.0, 2.0),
+                          ),
+                        ],
+                      ),
+                      textAlign: TextAlign.center, // Center the text
+                    ),
+                  ),
                 ),
                 const SizedBox(height: 16), // Add spacing below the counter
                 Text(
-                  randomText, // Display the random text
-                  style: Theme.of(context).textTheme.headlineSmall?.copyWith(fontStyle: FontStyle.italic), // Make text italic
+                  randomText, // Pastikan randomText tetap ada
+                  style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                    fontStyle: FontStyle.italic, // Make text italic
+                    color: Colors.black, // Change text color to black
+                  ),
                   textAlign: TextAlign.center, // Center the text
                 ),
               ],
@@ -101,6 +160,8 @@ class _TasbihScreenState extends State<TasbihScreen> { // Ganti State class
               onPressed: incrementCounter,
               tooltip: 'Increment',
               child: const Icon(Icons.add),
+              backgroundColor: Colors.blueAccent, // Change background color to blue accent
+              elevation: 5, // Increase elevation
             ),
           ),
         ],
